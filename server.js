3ee128e6baf04f214b0a5bc57a3c9c684114f091
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-14 17:53:59 -0500

    added guess function, lives function, animate function

--- server.js
+++ server.js
@@ -22,3 +22,76 @@ window.onload = function () {
     var getHint = document.getElementById("hint");
     var showClue = document.getElementById("clue");
 
+
+// Creating alphabet ul
+    var buttons = function () {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
+
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('li');
+            list.id = 'letter';
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    };
+
+
+    // Select Category
+    var selectCat = function () {
+        if (chosenCategory === categories[0]) {
+            catagoryName.innerHTML = "Your chosen category is NBA Teams";
+        } else if (chosenCategory === categories[1]) {
+            catagoryName.innerHTML = "Your chosen category is Music";
+        } else if (chosenCategory === categories[2]) {
+            catagoryName.innerHTML = "Your chosen category is Cities";
+        }
+    };
+
+
+    // Create guesses ul
+    result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (word[i] === "-") {
+                guess.innerHTML = "-";
+                space = 1;
+            } else {
+                guess.innerHTML = "_";
+            }
+
+            geusses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
+
+
+    // Show lives
+    comments = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+            showLives.innerHTML = "Game Over";
+        }
+        for (var i = 0; i < geusses.length; i++) {
+            if (counter + space === geusses.length) {
+                showLives.innerHTML = "You Win!";
+            }
+        }
+    }
+
+    // Animate man
+    var animate = function () {
+        var drawMe = lives ;
+        drawArray[drawMe]();
+    }
+
+
